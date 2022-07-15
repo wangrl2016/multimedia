fae14137f4dedfc8d87398cfc2d44efbf3b5834d
wangrl2016 <wangrl2016@gmail.com>
2022-07-15 15:19:29 +0800

    Download res file

--- scripts/init_project.py
+++ scripts/init_project.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 
+import os.path
+import requests
+
+temp_dir = 'temp'
+
+res_urls = [
+    'https://filesamples.com/samples/audio/mp2/sample1.mp2',
+]
+
 if __name__ == '__main__':
-    pass
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+    for url in res_urls:
+        filename = url.split('/')[-1]
+        url_path = os.path.join(os.getcwd(), os.path.join(temp_dir, filename))
+        if not os.path.exists(url_path):
+            r = requests.get(url, allow_redirects=True)
+            open(url_path, 'wb').write(r.content)
