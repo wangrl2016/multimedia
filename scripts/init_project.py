6068ed5b91cba27a97ed3fd8c005518a92687311
wangrl2016 <wangrl2016@gmail.com>
2022-07-15 12:00:44 +0800

    Download third source code

--- scripts/init_project.py
+++ scripts/init_project.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 import os
+import requests
+
+DOWNLOAD_FILES = {
+    'build/build_config.h': 'https://raw.githubusercontent.com/chromium/chromium/main/build/build_config.h',
+    'build/buildflag.h': 'https://raw.githubusercontent.com/chromium/chromium/main/build/buildflag.h',
+}
 
 if __name__ == '__main__':
-
-    print(os.getcwd())
-    pass
+    for file in DOWNLOAD_FILES.keys():
+        file_path = os.path.join(os.getcwd(), file)
+        if not os.path.exists(file_path):
+            r = requests.get(DOWNLOAD_FILES[file], allow_redirects=True)
+            open(file_path, 'wb').write(r.content)
