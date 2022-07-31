fe4d4cd1b3a3717f85ace6125c849628a3aca811
wangrl2016 <wangrl2016@gmail.com>
2022-07-31 10:09:58 +0800

    Write insert sort

---
+++ tutorials/algorithms/sorting/Insertion.java
@@ -0,0 +1,153 @@
+package sorting;
+
+import java.util.Comparator;
+
+public class Insertion {
+
+    // This class should not be instantiated.
+    private Insertion() {
+    }
+
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     *
+     * @param a the array to be sorted
+     */
+    public static void sort(Comparable[] a) {
+        int n = a.length;
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
+            }
+            assert isSorted(a, 0, i);
+        }
+        assert isSorted(a);
+    }
+
+    /**
+     * Rearranges the sub-array a[lo...hi) in ascending order, using the natural order.
+     *
+     * @param a  the array to be sorted
+     * @param lo left end point (inclusive)
+     * @param hi right end point (exclusive)
+     */
+    public static void sort(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i < hi; i++) {
+            for (int j = i; j > lo && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
+            }
+        }
+        assert isSorted(a, lo, hi);
+    }
+
+    /**
+     * Rearranges the array in ascending order, using a comparator.
+     *
+     * @param a          the array
+     * @param comparator the comparator specifying the order
+     */
+    public static void sort(Object[] a, Comparator comparator) {
+        int n = a.length;
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j - 1], comparator); j--) {
+                exch(a, j, j - 1);
+            }
+            assert isSorted(a, 0, i, comparator);
+        }
+        assert isSorted(a, comparator);
+    }
+
+    /**
+     * Rearranges the sub-array a[lo...hi) in ascending order, using a comparator.
+     *
+     * @param a          the array
+     * @param lo         left endpoint (inclusive)
+     * @param hi         right endpoint (exclusive)
+     * @param comparator the comparator specifying the order
+     */
+    public static void sort(Object[] a, int lo, int hi, Comparator comparator) {
+        for (int i = lo + 1; i < hi; i++) {
+            for (int j = i; j > lo && less(a[j], a[j - 1], comparator); j--) {
+                exch(a, j, j - 1);
+            }
+        }
+        assert isSorted(a, lo, hi, comparator);
+    }
+
+    /**
+     * Return a permutation that gives the elements in a[] in ascending order.
+     * do not change the original array a[]
+     * @param a the array
+     * @return a permutation p[] such that a[p[0]], a[p[1]], a[p[n-1]] are in ascending order
+     */
+    public static int[] indexSort(Comparable[] a) {
+        int n = a.length;
+        int[] index = new int[n];
+        for (int i = 0; i < n; i++)
+            index[i] = i;
+
+        for (int i = 1; i < n; i++)
+            for (int j = i; j > 0 && less(a[index[j]], a[index[j - 1]]); j--)
+                exch(index, j, j - 1);
+
+        return index;
+    }
+
+    // is v < w ?
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    // is v < w ?
+    private static boolean less(Object v, Object w, Comparator comparator) {
+        return comparator.compare(v, w) < 0;
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    // exchange a[i] and a[j] (for indirect sort)
+    private static void exch(int[] a, int i, int j) {
+        int swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length);
+    }
+
+    // is the array a[lo..hi) sorted
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i < hi; i++)
+            if (less(a[i], a[i - 1]))
+                return false;
+        return true;
+    }
+
+    private static boolean isSorted(Object[] a, Comparator comparator) {
+        return isSorted(a, 0, a.length, comparator);
+    }
+
+    // is the array a[lo..hi) sorted
+    private static boolean isSorted(Object[] a, int lo, int hi, Comparator comparator) {
+        for (int i = lo + 1; i < hi; i++)
+            if (less(a[i], a[i - 1], comparator))
+                return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+        Insertion.sort(a);
+
+        for (String s : a) {
+            System.out.print(s);
+            System.out.print(' ');
+        }
+    }
+}
